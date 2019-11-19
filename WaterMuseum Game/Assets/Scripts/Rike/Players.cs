64441efc4c28b3cddd17d688a22cd0a0ecf84166
Rik <rik-prins@hotmail.com>
2019-11-19 16:54:14 +0100

    Geld

--- WaterMuseum Game/Assets/Scripts/Rike/Players.cs
+++ WaterMuseum Game/Assets/Scripts/Rike/Players.cs
@@ -6,7 +6,6 @@ using UnityEngine;
 public class Players : MonoBehaviour
 {
     [SerializeField] private Text player1, player2, player3, player4;
-    private int player;
     private float money;
 
     private void Start()
@@ -16,12 +15,25 @@ public class Players : MonoBehaviour
     private void Update()
     {
         if (name == "Player 1")
+        {
             money += 0.1f * Time.deltaTime;
+            player1.text = "Geld: " + money;
+        }
+
         if (name == "Player 2")
+        {
             money += 0.2f * Time.deltaTime;
+            player2.text = "Geld: " + money;
+        }
         if (name == "Player 3")
+        {
             money += 0.3f * Time.deltaTime;
+            player3.text = "Geld: " + money;
+        }
         if (name == "Player 4")
+        {
             money += 0.4f * Time.deltaTime;
+            player4.text = "Geld: " + money;
+        }
     }
 }

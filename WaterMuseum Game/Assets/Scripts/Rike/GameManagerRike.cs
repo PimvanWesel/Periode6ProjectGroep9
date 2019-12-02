381329303c4865544ac90e90614d378184a47d7a
Rik <rik-prins@hotmail.com>
2019-12-02 12:30:10 +0100

    Spawn net

--- WaterMuseum Game/Assets/Scripts/Rike/GameManagerRike.cs
+++ WaterMuseum Game/Assets/Scripts/Rike/GameManagerRike.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class GameManagerRike : MonoBehaviour
 {
+    [SerializeField] private GameObject Vangnet;
     private GameObject player1;
     private GameObject player2;
     private GameObject player3;
     private GameObject player4;
+
+    private GameObject SpawnsPlayer1;
+    private GameObject SpawnsPlayer2;
+    private GameObject SpawnsPlayer3;
+    private GameObject SpawnsPlayer4;
+
     private TouchPhase phase;
 
     private bool onPlayer1;
@@ -21,6 +28,11 @@ public class GameManagerRike : MonoBehaviour
         player2 = GameObject.Find("Player 2");
         player3 = GameObject.Find("Player 3");
         player4 = GameObject.Find("Player 4");
+
+        SpawnsPlayer1 = GameObject.Find("Player 1 ObjectSpawns");
+        SpawnsPlayer2 = GameObject.Find("Player 2 ObjectSpawns");
+        SpawnsPlayer3 = GameObject.Find("Player 3 ObjectSpawns");
+        SpawnsPlayer4 = GameObject.Find("Player 4 ObjectSpawns");
     }
 
     private void Update()
@@ -34,71 +46,6 @@ public class GameManagerRike : MonoBehaviour
 
         //    if (phase != TouchPhase.Stationary)
         //    {
-        //        if (Physics.Raycast(raycast, out raycastHit))
-        //        {
-        //            if (raycastHit.collider.name == "Player 1")
-        //            {
-        //                if (onPlayer1 == true)
-        //                {
-        //                    player1.transform.GetChild(0).gameObject.SetActive(false);
-        //                    onPlayer1 = false;
-        //                }
-        //                else
-        //                {
-        //                    player1.transform.GetChild(0).gameObject.SetActive(true);
-
-        //                    onPlayer1 = true;
-        //                }
-        //            }
-
-        //            if (raycastHit.collider.name == "Player 2")
-        //            {
-        //                if (onPlayer2 == true)
-        //                {
-        //                    player2.transform.GetChild(0).gameObject.SetActive(false);
-        //                    onPlayer2 = false;
-        //                }
-        //                else
-        //                {
-        //                    player2.transform.GetChild(0).gameObject.SetActive(true);
-
-        //                    onPlayer2 = true;
-        //                }
-        //            }
-
-        //            if (raycastHit.collider.name == "Player 3")
-        //            {
-        //                if (onPlayer3 == true)
-        //                {
-        //                    player3.transform.GetChild(0).gameObject.SetActive(false);
-        //                    onPlayer3 = false;
-        //                }
-        //                else
-        //                {
-        //                    player3.transform.GetChild(0).gameObject.SetActive(true);
-
-        //                    onPlayer3 = true;
-        //                }
-        //            }
-
-        //            if (raycastHit.collider.name == "Player 4")
-        //            {
-        //                if (onPlayer4 == true)
-        //                {
-        //                    player4.transform.GetChild(0).gameObject.SetActive(false);
-        //                    onPlayer4 = false;
-        //                }
-        //                else
-        //                {
-        //                    player4.transform.GetChild(0).gameObject.SetActive(true);
-
-        //                    onPlayer4 = true;
-        //                }
-        //            }
-        //        }
-        //    }
-        //}
-
         if (Input.GetMouseButtonUp(0))
         {
             Ray raycast = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -164,6 +111,11 @@ public class GameManagerRike : MonoBehaviour
 
                         onPlayer4 = true;
                     }
+                }
+
+                if (raycastHit.collider.name == "WheelPlayer1")
+                {
+                    Instantiate(Vangnet, SpawnsPlayer1.transform.GetChild(0).transform.position, Quaternion.identity);
                 }
             }
         }

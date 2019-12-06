48db1d4f025eb2c3354f4d7fa3395ff3f053d360
Rik <rik-prins@hotmail.com>
2019-12-06 11:15:10 +0100

    SelectField

--- WaterMuseum Game/Assets/Scripts/Rike/Fade.cs
+++ WaterMuseum Game/Assets/Scripts/Rike/Fade.cs
@@ -2,13 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shop : MonoBehaviour
+public class Fade : MonoBehaviour
 {
     public float colora = 1;
+    private bool fulla;
 
     private void Update()
     {
-        colora -= 0.1f * Time.deltaTime;
+        if (colora <= 0)
+            fulla = false;
+        if (colora >= 1)
+            fulla = true;
+
+        if (fulla)
+        {
+            colora -= 1f * Time.deltaTime;
+        }
+        if (!fulla)
+        {
+            colora += 1f * Time.deltaTime;
+        }
+
         GetComponent<Renderer>().material.color = new Color(GetComponent<Renderer>().material.color.r, GetComponent<Renderer>().material.color.g, GetComponent<Renderer>().material.color.b, colora);
     }
 }

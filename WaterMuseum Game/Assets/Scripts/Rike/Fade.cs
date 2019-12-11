015aa37eb28a04c9ea95eaebfaf4371568e98d8f
Rik <rik-prins@hotmail.com>
2019-12-11 17:24:59 +0100

    Plaats player 1 en player 2 objects op spawnplek

--- WaterMuseum Game/Assets/Scripts/Rike/Fade.cs
+++ WaterMuseum Game/Assets/Scripts/Rike/Fade.cs
@@ -4,8 +4,29 @@ using UnityEngine;
 
 public class Fade : MonoBehaviour
 {
-    public float colora = 1;
+    private float colora = 1, colora2 = 1;
     private bool fulla;
+    private MeshRenderer[] r_Player1, r_Player2, r_Player3, r_Player4;
+
+    private void Start()
+    {
+        if (name == "FieldsPlayer1")
+        {
+            r_Player1 = GetComponentsInChildren<MeshRenderer>();
+        }
+        if (name == "FieldsPlayer2")
+        {
+            r_Player2 = GetComponentsInChildren<MeshRenderer>();
+        }
+        if (name == "FieldsPlayer3")
+        {
+            r_Player3 = GetComponentsInChildren<MeshRenderer>();
+        }
+        if (name == "FieldsPlayer4")
+        {
+            r_Player4 = GetComponentsInChildren<MeshRenderer>();
+        }
+    }
 
     private void Update()
     {
@@ -23,6 +44,65 @@ public class Fade : MonoBehaviour
             colora += 1f * Time.deltaTime;
         }
 
-        GetComponent<Renderer>().material.color = new Color(GetComponent<Renderer>().material.color.r, GetComponent<Renderer>().material.color.g, GetComponent<Renderer>().material.color.b, colora);
+        if (GameManagerRike.Instance.ObjectSelectedPlayer1)
+        {
+            foreach (MeshRenderer rend in r_Player1)
+            {
+                rend.material.color = new Color(rend.material.color.r, rend.material.color.g, rend.material.color.b, colora);
+            }
+        }
+        else
+        {
+            foreach (MeshRenderer rend in r_Player1)
+            {
+                rend.material.color = new Color(rend.material.color.r, rend.material.color.g, rend.material.color.b, 0f);
+            }
+        }
+
+        if (GameManagerRike.Instance.ObjectSelectedPlayer2)
+        {
+            foreach (MeshRenderer rend in r_Player2)
+            {
+                Debug.LogError("Aleezoh");
+                rend.material.color = new Color(rend.material.color.r, rend.material.color.g, rend.material.color.b, colora);
+            }
+        }
+        else
+        {
+            foreach (MeshRenderer rend in r_Player2)
+            {
+                rend.material.color = new Color(rend.material.color.r, rend.material.color.g, rend.material.color.b, 0f);
+            }
+        }
+
+        if (GameManagerRike.Instance.ObjectSelectedPlayer3)
+        {
+            foreach (MeshRenderer rend in r_Player3)
+            {
+                rend.material.color = new Color(rend.material.color.r, rend.material.color.g, rend.material.color.b, colora);
+            }
+        }
+        else
+        {
+            foreach (MeshRenderer rend in r_Player3)
+            {
+                rend.material.color = new Color(rend.material.color.r, rend.material.color.g, rend.material.color.b, 0f);
+            }
+        }
+
+        if (GameManagerRike.Instance.ObjectSelectedPlayer4)
+        {
+            foreach (MeshRenderer rend in r_Player4)
+            {
+                rend.material.color = new Color(rend.material.color.r, rend.material.color.g, rend.material.color.b, colora);
+            }
+        }
+        else
+        {
+            foreach (MeshRenderer rend in r_Player4)
+            {
+                rend.material.color = new Color(rend.material.color.r, rend.material.color.g, rend.material.color.b, 0f);
+            }
+        }
     }
 }

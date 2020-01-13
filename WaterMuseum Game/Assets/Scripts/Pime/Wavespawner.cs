f7c67f29b5eef0cd472eba7acc2a14d761a2a9e4
Pim van Wesel <psvwesel01@gmail.com>
2020-01-13 11:18:16 +0100

    wave sirrrrrrrr

---
+++ WaterMuseum Game/Assets/Scripts/Pime/Wavespawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wavespawner : MonoBehaviour
+{
+    public static int enemiesAlive = 0;
+
+    public Wave[] waves;
+
+    public Transform spawnpoint;
+
+    public float timeBetweenWaves = 5f;
+    public float countdown = 2f;
+
+    private int waveIndex = 0;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
